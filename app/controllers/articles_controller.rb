0f5c6c14b3412b2c2e955ba51b1f2aca09422598
Jordan Kamin <jordanakamin@gmail.com>
2015-03-11 17:07:15 -0700

    adds validation to model, adds strong params to controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,6 +9,17 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    article = Article.new
+    article.title = params[:title]
+    article.body = params[:body]
+    article.save!
+    redirect_to root_path
   end
+
+private
+
+def article_params
+  params.require(:article).permit(:title, :body)
+end
 
 end
