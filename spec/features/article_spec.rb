609620b2c1da5d7537b5ce49dee28ff4ceddbe0c
Jordan Kamin <jordanakamin@gmail.com>
2015-03-14 18:03:54 -0700

    tests create button

--- spec/features/article_spec.rb
+++ spec/features/article_spec.rb
@@ -7,4 +7,12 @@ describe "Article", :js => true do
     visit root_path
     expect(page).to have_content(article.title)
   end
+
+  it "creates a new post" do
+    visit root_path
+    fill_in 'Title', with: article.title
+    fill_in 'Body', with: article.body
+    click_on('Create Article')
+    expect(page).to have_content(article.body)
+  end
 end
