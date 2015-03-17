ddfd1308bd854d7a97b6ea3e3ce54eded542ee07
Jordan Kamin <jordanakamin@gmail.com>
2015-03-16 21:12:34 -0700

    tests the update button

--- spec/features/article_spec.rb
+++ spec/features/article_spec.rb
@@ -7,12 +7,22 @@ describe "Article", :js => true do
     visit root_path
     expect(page).to have_content(article.title)
   end
-
-  it "creates a new post" do
-    visit root_path
-    fill_in 'Title', with: article.title
-    fill_in 'Body', with: article.body
-    click_on('Create Article')
-    expect(page).to have_content(article.body)
+  context "Create Button" do
+    it "creates a new post" do
+        visit root_path
+        fill_in 'Title', with: article.title
+        fill_in 'Body', with: article.body
+        click_on('Create Article')
+        expect(page).to have_content(article.body)
+    end
+  end
+  context "Update Button" do
+    it "updates an existing post" do
+      new_title = "Howby Bowby"
+      visit article_path(article.id)
+      fill_in('Title', with: new_title)
+      click_on('Update')
+      expect(page).to have_content(new_title)
+    end
   end
 end
